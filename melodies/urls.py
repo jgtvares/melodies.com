9ed2148cf7994f18760882a07fe92ccdf4982583
Joao Tavares <joaogabrielsilvatavares@gmail.com>
2019-11-15 19:58:20 -0300

    add path to include app urls

--- melodies/urls.py
+++ melodies/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('website.urls')),
+    path('', include('website.urls', namespace='website')),
     path('admin/', admin.site.urls),
 ]
