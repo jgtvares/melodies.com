777aa2ed7049443e5fa88b1ba6dffac7ec4d417b
Joao Tavares <joaogabrielsilvatavares@gmail.com>
2019-11-15 21:28:56 -0300

    Add a view to render home page

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest, request
+
+import jinja2
 
 # Create your views here.
+def home(request):
+    return render(request, 'website/home.html')
